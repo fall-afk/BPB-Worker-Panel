8b212281bdb8fff0eee7fd0ca0123face40430f4
fall-afk <fakh.omid0@gmail.com>
2024-10-12 13:28:33 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b1f940cc-efb3-4fe2-a3c7-ab709ac8ebad';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
